eb0693e64d3b3d189e659ff91beaaf050589ba55
Gijs <79806264+Gijs6@users.noreply.github.com>
2025-02-08 18:08:49 +0100

    Add all the pages

--- main.py
+++ main.py
@@ -7,3 +7,40 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("ckvhome.html")
+
+@app.route("/over-mij")
+def over_mij():
+    return render_template("ckvovermij.html")
+
+@app.route("/introductielessen")
+def introductie():
+    return render_template("ckvintro.html")
+
+@app.route("/eigen-initiatieven")
+def eigen_initiatieven():
+    return render_template("ckvei.html")
+
+@app.route("/blok-1")
+def blok1():
+    return render_template("ckvb1.html")
+
+@app.route("/blok-2")
+def blok2():
+    return render_template("ckvb2.html")
+
+@app.route("/blok-3")
+def blok3():
+    return render_template("ckvb3.html")
+
+@app.route("/blok-4")
+def blok4():
+    return render_template("ckvb4.html")
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("ckv404.html", e=e), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('ckv500.html', e=e), 500
