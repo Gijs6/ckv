552cc3ce934a17bfb5cca4c7bd4befb36df049e3
Gijs <79806264+Gijs6@users.noreply.github.com>
2025-04-25 14:45:40 +0200

    I hate this

--- main.py
+++ main.py
@@ -22,13 +22,8 @@ def redirecter(path):
         return redirect(f"/{path}")
     elif not path.startswith("gijstenberg4a2"):
         return redirect(f"/gijstenberg4a2/{path}")
-    try:
-        imgurl, artwork, artist, txtcolor = randomBackground()
-    except Exception:
-        imgurl = "/static/imgs/error/cirkels2.jpg"
-        artwork = "Primordial Chaos - No 16 (1906-1907)"
-        artist = "Hilma af Klint"
-        txtcolor = "white"
+    
+    imgurl, artwork, artist, txtcolor = randomBackground()
     return render_template("error.html", e="Error redirecting", errornum="404", message1="Die pagina bestaat niet!", message2="De URL die je hebt verzocht bestaat niet.", imgurl=imgurl, artwork=artwork, artist=artist, txtcolor=txtcolor), 404
 
 
@@ -186,33 +181,24 @@ def period_4():
 # Error pages
 
 def randomBackground():
-    with open(os.path.join(BASE_DIR, "data", "backgroundlist.json"), "r") as file:
-        data = json.load(file)
-    randomBackgroundChoice = random.choice(data)
+    try:
+        with open(os.path.join(BASE_DIR, "data", "backgroundlist.json"), "r") as file:
+            data = json.load(file)
+        randomBackgroundChoice = random.choice(data)
 
-    return randomBackgroundChoice.get("imgurl"), randomBackgroundChoice.get("artwork"), randomBackgroundChoice.get("artist"), randomBackgroundChoice.get("txtcolor")
+        return randomBackgroundChoice.get("imgurl"), randomBackgroundChoice.get("artwork"), randomBackgroundChoice.get("artist"), randomBackgroundChoice.get("txtcolor")
+    except Exception:
+        return "/static/imgs/error/Primordial_Chaos.jpg", "Primordial Chaos - No 16 (1906-1907)", "Hilma af Klint", "white"
 
 
 @app.errorhandler(404)
 def not_found(e):
-    try:
-        imgurl, artwork, artist, txtcolor = randomBackground()
-    except Exception:
-        imgurl = "/static/imgs/error/cirkels2.jpg"
-        artwork = "Primordial Chaos - No 16 (1906-1907)"
-        artist = "Hilma af Klint"
-        txtcolor = "white"
+    imgurl, artwork, artist, txtcolor = randomBackground()
     return render_template("error.html", e=e, errornum="404", message1="Die pagina bestaat niet!", message2="De URL die je hebt verzocht bestaat niet.", imgurl=imgurl, artwork=artwork, artist=artist, txtcolor=txtcolor), 404
 
 @app.errorhandler(500)
 def internal_error(e):
-    try:
-        imgurl, artwork, artist, txtcolor = randomBackground()
-    except Exception:
-        imgurl = "/static/imgs/error/cirkels2.jpg"
-        artwork = "Primordial Chaos - No 16 (1906-1907)"
-        artist = "Hilma af Klint"
-        txtcolor = "white"
+    imgurl, artwork, artist, txtcolor = randomBackground()
     return render_template("error.html", e=e, errornum="404", message1="Oeps! Er is iets misgegaan", message2="Er lijkt een probleem te zijn op de server. De pagina kon niet worden geladen. Probeer het later nog eens.", imgurl=imgurl, artwork=artwork, artist=artist, txtcolor=txtcolor), 505
 
 
