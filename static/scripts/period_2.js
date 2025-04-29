0e624fa2fe0c34f30f96b343f97f06513c6bb811
Gijs <79806264+Gijs6@users.noreply.github.com>
2025-04-29 10:59:22 +0200

    I couldn't hate JS and CSS more

--- static/scripts/period_2.js
+++ static/scripts/period_2.js
@@ -1,40 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".tile").forEach(tile => {
-        const tileContent = tile.querySelector(".tile-content");
-
-
-        const width = tile.offsetWidth + "px";
-        const height = tile.offsetHeight + "px";
-
-
+    setTimeout(() => {
+        document.querySelectorAll(".tile").forEach(tile => {
+            const tileContent = tile.querySelector(".tile-content");
+            
+    
+            const width = tile.offsetWidth + "px";
+            const height = tile.offsetHeight + "px";
         
-        console.log(width)
-        console.log(height)
         
-        tile.style.width = width;
-        tile.style.height = height;
+            tile.style.width = width;
+            tile.style.height = height;
 
-        tile.addEventListener("click", () => {
-            if (!tile.classList.contains("tile-active")) {
-                tile.classList.add("tile-active");
+            tile.addEventListener("click", () => {
+                if (!tile.classList.contains("tile-active")) {
+                    tile.classList.add("tile-active");
 
-                tile.style.width = "100%";
-                tile.style.height = "100%";
-            }
+                    tile.style.width = "100%";
+                    tile.style.height = "100%";
+                }
+            });
+
+            const closeButton = tile.querySelector(".close-tile");
+
+            closeButton.addEventListener("click", () => {
+                tileContent.style.animation = "250ms fadeOut forwards";
+                closeButton.style.animation = "250ms fadeOut forwards";
+                setTimeout(() => {
+                    tile.classList.remove("tile-active");
+                    tile.style.width = width;
+                    tile.style.height = height;
+                    tileContent.style.removeProperty("animation");
+                    closeButton.style.removeProperty("animation");
+                }, 250);
+            });
         });
-
-        const closeButton = tile.querySelector(".close-tile");
-
-        closeButton.addEventListener("click", () => {
-            tileContent.style.animation = "250ms fadeOut forwards";
-            closeButton.style.animation = "250ms fadeOut forwards";
-            setTimeout(() => {
-                tile.classList.remove("tile-active");
-                tile.style.width = width;
-                tile.style.height = height;
-                tileContent.style.removeProperty("animation");
-                closeButton.style.removeProperty("animation");
-            }, 250);
-        });
-    });
+    }, 250);
 });
